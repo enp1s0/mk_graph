65efdd0748bd7f52d5e0e8df3e512c15a00dd1d7
mutsuki <mutsuki@momo86.net>
2021-05-30 18:11:52 +0900

    Add comment

--- simple_single_graph_from_csv/mk_graph.py
+++ simple_single_graph_from_csv/mk_graph.py
@@ -1,25 +1,35 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-output_file_extension = "pdf"
+# Output format
+output_file_extension = "png"
 
+# The list of `type` in the input csv file
 type_list = ['A', 'B']
 
+# Figure config
 plt.figure(figsize=(8, 3))
 plt.xlabel("X")
 plt.ylabel("Y")
 #plt.yscale("log", base=10)
 plt.grid()
 
+# Load input data
 df = pd.read_csv("data.csv", encoding="UTF-8")
 
 for t in type_list:
+    # Filter the input data
     df_t = df.query("type=='" + t + "'")
+
+    # Plot
     plt.plot(df_t['x'], df_t['y'], label="Type" + t, markersize=4, marker="*")
 
+# Legend config
 plt.legend(loc='best',
        bbox_to_anchor=(0, 1.3, 1., -.1),
        borderaxespad=0.,
        ncol=len(type_list),
        mode="expand")
+
+# Save to file
 plt.savefig("result." + output_file_extension, bbox_inches="tight")
