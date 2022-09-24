84561cd3d9617c8b0c91ec394a8fa8c52254debc
mutsuki <mutsuki@momo86.net>
2022-09-24 14:46:54 +0900

    Update simple_multi_graph_from_csv

--- simple_multi_graph_from_csv/mk_graph.py
+++ simple_multi_graph_from_csv/mk_graph.py
@@ -39,7 +39,7 @@ for i in range(fig_block_n):
                     markersize=4,
                     marker="*",
                     color=color_table[t])
-            if v == len(type_list):
+            if i == 0 and j == 0:
                 line_list += [l]
                 label_list += [t]
 
